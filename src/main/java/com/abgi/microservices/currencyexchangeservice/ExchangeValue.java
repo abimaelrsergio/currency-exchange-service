de43a0f800e9d7f44eb335a69646db32a89943d1
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-11-08 21:51:08 -0300

    Atualizacao

--- src/main/java/com/abgi/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/abgi/microservices/currencyexchangeservice/ExchangeValue.java
@@ -2,17 +2,29 @@ package com.abgi.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class ExchangeValue {
 
+	@Id
 	private Long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
+	
 	private BigDecimal conversionMultiple;
+	
 	private int port;
 
 	public ExchangeValue() {
